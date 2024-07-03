84f7cb2b057f55791b05a04026fd52ab38ed0512
stephanie <stephaniewan07@gmail.com>
2024-07-03 11:30:01 -0400

    rebuilt blog

---
+++ script.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const gallery = document.querySelector('#gallery');
+    let msnry; // Initialize Masonry variable
+  
+    // Initialize Masonry after all images have loaded
+    imagesLoaded(gallery, () => {
+        msnry = new Masonry(gallery, {
+            itemSelector: '.gallery-item',
+            percentPosition: true,
+            columnWidth: '.gallery-item',
+            gutter: 16
+        });
+    });
+  
+    // Function to filter gallery items
+    window.filterGallery = function(filter) {
+        const items = gallery.querySelectorAll('.gallery-item');
+  
+        items.forEach(item => {
+            item.classList.toggle('is-hidden', !(filter === 'all' || item.classList.contains(filter)));
+        }); 
+  
+        imagesLoaded(gallery, () => {
+          setTimeout(() => {
+            // msnry.reloadItems();
+            // msnry.layout();
+        }, 50);
+      });
+        
+        
+    };
+  });
+  
+  
+  
