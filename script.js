67074b23f7dccc5f1ca2df95797ee949fdfaad65
stephanie <stephaniewan07@gmail.com>
2024-11-03 21:50:58 -0500

    changed art gallery, removed masonry.js

--- script.js
+++ script.js
@@ -1,35 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const gallery = document.querySelector('#gallery');
-    let msnry; // Initialize Masonry variable
+function openModal(element) {
+  const modal = document.getElementById('modal');
+  const modalImg = document.getElementById('modalImage');
+  const captionText = document.getElementById('caption');
   
-    // Initialize Masonry after all images have loaded
-    imagesLoaded(gallery, () => {
-        msnry = new Masonry(gallery, {
-            itemSelector: '.gallery-item',
-            percentPosition: true,
-            columnWidth: '.gallery-item',
-            gutter: 16
-        });
-    });
-  
-    // Function to filter gallery items
-    window.filterGallery = function(filter) {
-        const items = gallery.querySelectorAll('.gallery-item');
-  
-        items.forEach(item => {
-            item.classList.toggle('is-hidden', !(filter === 'all' || item.classList.contains(filter)));
-        }); 
-  
-        imagesLoaded(gallery, () => {
-          setTimeout(() => {
-            // msnry.reloadItems();
-            // msnry.layout();
-        }, 50);
-      });
-        
-        
-    };
-  });
+  modal.style.display = "block";
+  modalImg.src = element.querySelector('.gallery-image').src;
+  captionText.innerHTML = element.querySelector('.gallery-item-text').textContent;
+}
+
+function closeModal() {
+  document.getElementById('modal').style.display = "none";
+}
+
   
   
   
